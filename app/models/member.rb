5445cd64e315392cf67bb456f556c3ead8782857
Junichi Ito <jit@sonicgarden.jp>
2015-02-11 21:00:01 +0900

    Add sample spec.

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,19 @@
 class Member < ActiveRecord::Base
   has_many :group_members
   has_many :groups, through: :group_members
+
+  def self.not_joined_to(event)
+    members = Member.arel_table
+    groups = Group.arel_table
+    groups_members = GroupMember.arel_table
+
+    condition =
+        groups
+            .project(Arel.star)
+            .join(groups_members)
+            .on(groups[:id].eq(groups_members[:group_id]), groups[:event_id].eq(event.id))
+            .where(members[:id].eq(groups_members[:member_id]))
+
+    Member.where(condition.exists.not).all
+  end
 end
--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Member, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '::not_joined_to' do
+    example do
+      event_yakiniku = create :event, name: '焼き肉パーティ'
+      group_men = create :group, name: '男性グループ', event: event_yakiniku
+      member_takashi = create :member, name: 'たかし'
+      create :group_member, group: group_men, member: member_takashi
+
+      event_karaoke = create :event, name: 'カラオケパーティ'
+      group_20s = create :group, name: '20代グループ', event: event_karaoke
+      member_sachiko = create :member, name: 'さちこ'
+      create :group_member, group: group_20s, member: member_sachiko
+
+      members = Member.not_joined_to(event_karaoke)
+      expect(members.count).to eq 1
+      expect(members.first).to eq member_takashi
+    end
+  end
 end
