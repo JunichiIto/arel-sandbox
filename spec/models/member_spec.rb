56423f040f9946a5b6b0f86d6eb57787b3cdfda0
Junichi Ito <jit@sonicgarden.jp>
2015-02-11 21:03:00 +0900

    Refine spec.

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -7,17 +7,23 @@ RSpec.describe Member, type: :model do
       group_men = create :group, name: '男性グループ', event: event_yakiniku
       member_takashi = create :member, name: 'たかし'
       create :group_member, group: group_men, member: member_takashi
+      group_women = create :group, name: '女性グループ', event: event_yakiniku
+      member_hiromi = create :member, name: 'ひろみ'
+      create :group_member, group: group_women, member: member_hiromi
 
       event_karaoke = create :event, name: 'カラオケパーティ'
       group_20s = create :group, name: '20代グループ', event: event_karaoke
       member_sachiko = create :member, name: 'さちこ'
       create :group_member, group: group_20s, member: member_sachiko
+      group_50s = create :group, name: '50代グループ', event: event_karaoke
+      member_yuzo = create :member, name: 'ゆうぞう'
+      create :group_member, group: group_50s, member: member_yuzo
 
       member_taro = create :member, name: 'たろう'
 
       members = Member.not_joined_to(event_karaoke)
-      expect(members.count).to eq 2
-      expect(members).to contain_exactly(member_takashi, member_taro)
+      expect(members.count).to eq 3
+      expect(members).to contain_exactly(member_takashi, member_hiromi, member_taro)
     end
   end
 end
